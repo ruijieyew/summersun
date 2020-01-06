1cd77d6d8a57fb5d7b23a92f161b5bd3884b792a
Rui-Jie Yew <ruijie.yew@gmail.com>
2020-01-06 12:13:28 -0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -10,6 +10,7 @@ function preload() {
 }
 
 function setup() {
+
   song.play();
   // create a new Amplitude analyzer
   fft = new p5.FFT();
@@ -20,10 +21,12 @@ function setup() {
 
 function mousePressed(){
   if (song.isPlaying()){
-    song.pause()
+    song.pause();
+    
   }
   else{
-    song.play()
+    song.play();
+    a = 0;
   }
 }
 
@@ -34,7 +37,7 @@ function draw() {
     noStroke();
   from = color(31,151,196);
   to = color(178,135,169);
-  c1 = lerpColor(from, to,map(a, 0, 190, 0, 1));
+  c1 = lerpColor(from, to,map(a, 0, 200, 0, 1));
   background(c1);
   fill(255);
   noStroke();
@@ -52,16 +55,16 @@ function draw() {
                 "at least I'll have the summer sun", "shining on me", "bask in glory", "feel the warmth of the summer sun", "boy if you could see what you've done to me","I will always remember that you're the one in my heart", "you cry in this summer sun", 
                 "I leave you here to cry", "cry", "cry"]
   for (var i = 0; i < lyrics.length; i++) {
-    text(lyrics[i],0, (height - (speed*a + 10)) + i*characterSpace)
+    text(lyrics[i],0, 215+15*i-0.47*a);
   }
   for (var i = 0; i < spectrum.length; i++) {
     var angle = map(i, 0, spectrum.length, 0, 360);
     var amp = spectrum[i];
     var r = map(amp, 0, 100, 50, 80);
-    fill(247-0.15*a,215-0.35*a,83-0.15*a);
+    fill(247-0.1*a,215-0.25*a,83-0.1*a);
     var x =  1.5*r * cos(angle);
     var y = 1.5*r * sin(angle);
-    stroke(247-0.15*a,215-0.35*a,83-0.15*a);
+    stroke(247-0.1*a,215-0.25*a,83-0.1*a);
     line(0, 0, x, y);
     noStroke();
   }
