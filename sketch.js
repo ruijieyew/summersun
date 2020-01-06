0f560b57b0c62c2e86eba738425df48951cc9992
Rui-Jie Yew <ruijie.yew@gmail.com>
2020-01-06 04:05:39 -0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,5 +1,6 @@
 var song;
 var fft;
+let a = 0;
 
 var volhistory = [];
 
@@ -14,7 +15,7 @@ function setup() {
   fft = new p5.FFT();
   song.amp(0.2);
   // Patch the input to an volume analyzer
-  createCanvas(500, 500);
+  createCanvas(500, 650);
 }
 
 function mousePressed() {
@@ -23,40 +24,48 @@ function mousePressed() {
     song.stop();
   } else {
     song.play();
+    a = 0;
   }
 }
 
 function draw() {
-  background(239,206,206);
+  if (song.isPlaying()) {
+    a = a + 0.05;
+  }
+  console.log(a);
+    noStroke();
+  from = color(31,151,196);
+  to = color(178,135,169);
+  c1 = lerpColor(from, to,map(a, 0, 190, 0, 1));
+  background(c1);
   fill(255);
   noStroke();
   var spectrum = fft.analyze();
   noStroke();
-  translate(width / 2, height / 1.2);
+  translate(width / 2, 0.75*a);
+  textSize(13);
+  textFont(fontBold);
+  textAlign(CENTER);
+  var speed = 2.48;
+  var characterSpace = 15;
+  var lyrics = ["summer sun", "vincent chiang","when I see you far away","you drift in like the summer sun", 
+                "know that you're the one", "but by the time I see", 
+                "you're not here for me, for me", "so do me no wrong","if I've got no one",
+                "at least I'll have the summer sun", "shining on me", "bask in glory", "feel the warmth of the summer sun", "boy if you could see", "what you've done to me","I will always remember", 
+                "that you're the one in my heart", "you cry", "in this summer sun", 
+                "I leave you here to cry", "cry", "cry"]
+  for (var i = 0; i < lyrics.length; i++) {
+    text(lyrics[i],0, (height - (speed*a + 10)) + i*characterSpace)
+  }
   for (var i = 0; i < spectrum.length; i++) {
     var angle = map(i, 0, spectrum.length, 0, 360);
     var amp = spectrum[i];
     var r = map(amp, 0, 100, 50, 80);
-    fill(237,121,71);
-    var x = 2.95 * r * cos(angle);
-    var y = 3 * r * sin(angle);
-    stroke(237,121,71);
+    fill(247-0.15*a,215-0.35*a,83-0.15*a);
+    var x =  1.5*r * cos(angle);
+    var y = 1.5*r * sin(angle);
+    stroke(247-0.15*a,215-0.35*a,83-0.15*a);
     line(0, 0, x, y);
     noStroke();
   }
-  //translate(width / 2, height / 2);
-  noStroke();
-  rectMode(CORNER);
-  fill(219,186,186);
-  rect(-250, -30, 500, 30);
-  fill(180,190,200);
-  rect(-250, -40, 500, 10);
-  fill(119,147,194);
-  rect(-250, -20, 500, 30);
-  fill(175,199,219);
-  rect(-250, -5, 500, 50);
-  fill(25, 45, 70);
-  rect(-250, 40, 500, 44);
-  fill(40, 60, 94);
-  fill(255);
 }
