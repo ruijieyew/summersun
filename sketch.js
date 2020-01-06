e7a5ec86924b14394b65a3b5633e8d740f484a13
Rui-Jie Yew <ruijie.yew@gmail.com>
2020-01-06 13:45:01 -0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,6 +1,7 @@
 var song;
 var fft;
 let a = 0;
+var songEnded = false;
 
 var volhistory = [];
 
@@ -10,40 +11,33 @@ function preload() {
 }
 
 function setup() {
-
-  song.play();
+  a = 0;
   // create a new Amplitude analyzer
   fft = new p5.FFT();
   song.amp(0.2);
-  // Patch the input to an volume analyzer
   createCanvas(500,500);
 }
 
+
 function mousePressed(){
-  if (song.isPlaying()){
-    song.pause();
-    
-  }
-  else{
+  if (!song.isPlaying()){
     song.play();
-    a = 0;
+  }
+  else if(song.currentTime() >= song.duration()){
+    setup();
   }
 }
 
 function draw() {
-  if (song.isPlaying()) {
-    a = a + 0.05;
-  }
-    noStroke();
+  noStroke();
   from = color(31,151,196);
   to = color(178,135,169);
-  c1 = lerpColor(from, to,map(a, 0, 200, 0, 1));
+  c1 = lerpColor(from, to,map(song.currentTime(), 0, 113.73133786848072, 0, 1));
   background(c1);
   fill(255);
-  noStroke();
   var spectrum = fft.analyze();
   noStroke();
-  translate(width / 2, 0.47*a);
+  translate(width / 2, song.currentTime());
   textSize(13);
   textFont(fontBold);
   textAlign(CENTER);
@@ -55,16 +49,16 @@ function draw() {
                 "at least I'll have the summer sun", "shining on me", "bask in glory", "feel the warmth of the summer sun", "boy if you could see what you've done to me","I will always remember that you're the one in my heart", "you cry in this summer sun", 
                 "I leave you here to cry", "cry", "cry"]
   for (var i = 0; i < lyrics.length; i++) {
-    text(lyrics[i],0, 215+15*i-0.47*a);
+    text(lyrics[i],0, 215+15*i-song.currentTime());
   }
   for (var i = 0; i < spectrum.length; i++) {
     var angle = map(i, 0, spectrum.length, 0, 360);
     var amp = spectrum[i];
     var r = map(amp, 0, 100, 50, 80);
-    fill(247-0.1*a,215-0.25*a,83-0.1*a);
+    fill(247-0.1*song.currentTime(),215-0.25*song.currentTime(),83-0.1*song.currentTime());
     var x =  1.5*r * cos(angle);
     var y = 1.5*r * sin(angle);
-    stroke(247-0.1*a,215-0.25*a,83-0.1*a);
+    stroke(247-0.1*song.currentTime(),215-0.25*song.currentTime(),83-0.1*song.currentTime());
     line(0, 0, x, y);
     noStroke();
   }
